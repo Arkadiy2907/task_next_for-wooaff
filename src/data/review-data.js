f01fa0416fbeab42def67e726cb6e15d3daa4441
Arkadiy2907 <arkadiy2907@gmail.com>
2023-10-08 18:14:48 +0300

    added change for tasks

---
+++ src/data/review-data.js
@@ -0,0 +1,25 @@
+const review_content = [
+    {
+        id: 1,
+        img: "/assets/img/icon/c-details-avata-01.png",
+        name: "Brooklyn Simmons",
+        review_text: <>Foster leadership around the clock in a synergistic manner rather than on scalable platforms. Easily visualize existing product installations prior to dynamic outcomes. Collaboratively revive corporate experiences and open-source applications.</>,
+        review_time: "a week ago"
+    },
+    {
+        id: 2,
+        img: "/assets/img/icon/c-details-avata-02.png",
+        name: "Leslie Alexander",
+        review_text: <>Foster leadership around the clock in a synergistic manner rather than on scalable platforms. Easily visualize existing product installations prior to dynamic outcomes. Collaboratively revive corporate experiences and open-source applications.</>,
+        review_time: "a week ago"
+    },
+    {
+        id: 3,
+        img: "/assets/img/icon/c-details-avata-03.png",
+        name: "Dianne Russell",
+        review_text: <>Foster leadership around the clock in a synergistic manner rather than on scalable platforms. Easily visualize existing product installations prior to dynamic outcomes. Collaboratively revive corporate experiences and open-source applications.</>,
+        review_time: "a week ago"
+    },
+];
+
+export default review_content;
